57e5506434881f4fd952df2feadaa15e5d3bba38
OhalViraj <er.ohalviraj@gmail.com>
2025-09-25 13:55:41 +0530

    pagination on user products done

--- src/main/java/com/ecom/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecom/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecom.service.impl;
 
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,6 +10,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -111,6 +115,23 @@ public class ProductServiceImpl implements ProductService {
 		return productRepository.findByTitleContainingIgnoreCaseOrCategoryContainingIgnoreCase(ch, ch);
 		
 		
+	}
+
+	@Override
+	public Page<Product> getAllActiveProductPagination(Integer pageNo, Integer pageSize,String category) {
+		// TODO Auto-generated method stub
+		Pageable pageable= PageRequest.of(pageNo, pageSize);
+
+		Page<Product> pageProduct= null;
+	
+		if (ObjectUtils.isEmpty(category)) {
+			pageProduct= productRepository.findByIsActiveTrue(pageable);
+		}else {
+			pageProduct=productRepository.findByCategory(pageable,category);
+		}
+
+		
+		return pageProduct;
 	}
 
 	
